9accb73f6a087a39daf74e2e49548f9ecc1d5466
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-12-05 22:23:07 -0700

    Save the schema from oblivion

--- spec/requests/api/v0/market_request_spec.rb
+++ spec/requests/api/v0/market_request_spec.rb
@@ -14,6 +14,49 @@ describe "Markets API" do
 
     markets.each do |market|
       expect(market).to have_key(:id)
+      expect(market[:id]).to be_an(String)
+
+      attributes = market[:attributes]
+      expect(attributes).to have_key(:name)
+      expect(attributes[:name]).to be_a(String)
+
+      expect(attributes).to have_key(:street)
+      expect(attributes[:street]).to be_a(String)
+
+      expect(attributes).to have_key(:city)
+      expect(attributes[:city]).to be_a(String)
+
+      expect(attributes).to have_key(:county)
+      expect(attributes[:county]).to be_a(String)
+
+      expect(attributes).to have_key(:state)
+      expect(attributes[:state]).to be_an(String)
+      expect(attributes[:state].length).to eq(2)
+
+      expect(attributes).to have_key(:zip)
+      expect(attributes[:zip]).to be_a(String)
+
+      expect(attributes).to have_key(:lat)
+      expect(attributes[:lat]).to be_a(String)
+
+      expect(attributes).to have_key(:lon)
+      expect(attributes[:lon]).to be_a(String)
+    end
+  end
+
+  it "sends attributes for a single market" do
+    create_list(:market, 3)
+
+    get "/api/v0/markets/#{Market.first.id}"
+
+    expect(response).to be_successful
+
+    markets = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(markets.count).to eq(3)
+
+    markets.each do |market|
+      expect(market).to have_key(:id)
       expect(market[:id]).to be_an(Integer)
 
       expect(market).to have_key(:title)
